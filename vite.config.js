e2030aa3b021faf1f0d7fb9599b874f7d7e2d27a
poliweb <poliweb007@gmail.com>
2022-02-22 08:13:11 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -10,11 +10,29 @@ export default defineConfig({
     VitePWA({
       registerType: 'autoUpdate',
       includeAssets: ['favicon.svg', 'robots.txt', 'safari-pinned-tab.svg'],
-      workbox: {
-        globPatterns: [],
-        cleanupOutdatedCaches: false,
-        sourcemap: true,
-      }
+      manifest: {
+        name: 'Vitesse',
+        short_name: 'Vitesse',
+        theme_color: '#db2777',
+        icons: [
+          {
+            src: '/pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+          {
+            src: '/pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+            purpose: 'any maskable',
+          },
+        ],
+      },
     }),
   ],
   resolve: {
