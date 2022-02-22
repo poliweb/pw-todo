38991bfb4c25d66b9bbf0afbbea0fff1a5466ee7
poliweb <poliweb007@gmail.com>
2022-02-22 10:07:00 +0600

    Update index.html and src/components/Footer.vue

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <footer class="bg-pink-100 body-font">
   <div class="container px-5 py-8 mx-auto flex items-center sm:flex-row flex-col">
-    <a class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
+    <router-link :to="{name: 'home'}" class="flex title-font font-medium items-center md:justify-start justify-center text-gray-900">
       <IconLogo class="w-24"/>
-    </a>
+    </router-link> 
     <p class="text-sm text-gray-500 sm:ml-4 sm:pl-4 sm:border-l-2 sm:border-gray-200 sm:py-2 sm:mt-0 mt-4">© {{ new Date().getFullYear() }}  Web Developer —
       <a href="https://github.com/poliweb" class="text-pink-600 ml-1" rel="noopener noreferrer" target="_blank">@poliweb</a>
     </p>
