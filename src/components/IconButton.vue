a8de7e48a5a17c2d9b1ffae1bbda9edc1f6d70b2
poliweb <poliweb007@gmail.com>
2022-02-24 12:15:34 +0600

    Update src/components/IconButton.vue

--- src/components/IconButton.vue
+++ src/components/IconButton.vue
@@ -2,8 +2,8 @@
   <div class="">
       <ul class="flex flex-wrap items-center space-x-3">
           <li class=""><a href="https://github.com/poliweb" target="_blank" rel="noreferrer" aria-label="Follow me on Github" class="text-pink-600 hover:text-pink-500 "><IconGithub/></a></li>
-          <li class=""><a href="https://dribbble.com/PoliWeb" target="_blank" rel="noreferrer" aria-label="Follow me on Dribbble" class="text-pink-600 hover:text-pink-500 "><IconDribbble/></a></li>
           <li class=""><a href="https://codepen.io/poliweb" target="_blank" rel="noreferrer" aria-label="Follow me on Codepen" class="text-pink-600 hover:text-pink-500 "><IconCodepen/></a></li>
+          <li class=""><a href="https://dribbble.com/PoliWeb" target="_blank" rel="noreferrer" aria-label="Follow me on Dribbble" class="text-pink-600 hover:text-pink-500 "><IconDribbble/></a></li>
           <li class=""><a href="https://www.behance.net/poliweb" target="_blank" rel="noreferrer" aria-label="Follow me on Behance" class="text-pink-600 hover:text-pink-500 "><IconBehance/></a></li>
           <li class=""><a href="https://www.linkedin.com/in/poliweb/" target="_blank" rel="noreferrer" aria-label="Follow me on Linkedin" class="text-pink-600 hover:text-pink-500 "><IconLinkedin/></a></li>
       </ul>
@@ -13,7 +13,7 @@
 <script>
 import IconGithub from "./icons/IconGithub.vue";
 import IconDribbble from "./icons/IconDribbble.vue";
-import IconCodepen from "./icons/IconGithub.vue";
+import IconCodepen from "./icons/IconCodepen.vue";
 import IconBehance from "./icons/IconBehance.vue";
 import IconLinkedin from "./icons/IconLinkedin.vue";
 export default {
