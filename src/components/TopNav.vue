3d464d6518b0fb2554f2ce597bb8481535e6e3c2
poliweb <poliweb007@gmail.com>
2022-02-24 09:04:05 +0600

    Update icons src/components/icons/IconBehance.vue and src/components/TopNav.vue

--- src/components/TopNav.vue
+++ src/components/TopNav.vue
@@ -3,12 +3,12 @@ import { RouterLink, RouterView } from "vue-router";
 </script>
 <template>
   <header class="text-gray-600 body-font">
-  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+  <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center space-y-4">
     <router-link :to="{name: 'home'}" aria-label="To Home" class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0">
       <IconLogo/>
     </router-link>
-    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
-      <router-link :to="{name: 'home'}" replace class="mr-5 hover:text-gray-900">Home</router-link>
+    <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-lg md:text-base justify-center">
+      <router-link :to="{name: 'home'}" replace class="mr-5 hover:text-gray-900 ">Home</router-link>
       <router-link :to="{name: 'about'}" class="mr-5 hover:text-gray-900">About Me</router-link>
       <router-link :to="{name: 'contact'}" class="mr-5 hover:text-gray-900">Сontact</router-link>
 
