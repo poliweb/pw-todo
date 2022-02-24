228a97a06a961b1b462ccec434c5918eb370719e
poliweb <poliweb007@gmail.com>
2022-02-24 06:00:36 +0600

    Update manifest in vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -15,23 +15,31 @@ export default defineConfig({
         short_name: 'PW-ToDo',
         description: 'To Do поможет вам сосредоточиться на любом занятии — от работы до подготовки к путишествию.',
         theme_color: '#db2777',
+        "background_color": "#f9e2ed",
+        "display": "standalone",
+        "scope": "/",
+        "start_url": "/",
         icons: [
           {
-            src: '/pwa-192x192.png',
-            sizes: '192x192',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-          },
-          {
-            src: '/pwa-512x512.png',
-            sizes: '512x512',
-            type: 'image/png',
-            purpose: 'any maskable',
-          },
+            "src": "/icon-192x192.png",
+            "sizes": "192x192",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-256x256.png",
+            "sizes": "256x256",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-384x384.png",
+            "sizes": "384x384",
+            "type": "image/png"
+        },
+        {
+            "src": "/icon-512x512.png",
+            "sizes": "512x512",
+            "type": "image/png"
+        }
         ],
       },
     }),
