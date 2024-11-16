a7a9b54fb5d670f20895da1292148ca406d3a05b
poliweb <web@poliweb.su>
2024-11-16 14:14:33 +0600

    :update

--- src/views/username/PostDetailView.vue
+++ src/views/username/PostDetailView.vue
@@ -1,82 +1,45 @@
 <template>
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-col">
-      <div
-        v-if="article"
-        class="lg:w-5/6 mx-auto"
-      >
+      <div v-if="article" class="lg:w-5/6 mx-auto">
         <div class="rounded-lg overflow-hidden">
-          <img
-            v-if="article.cover_image"
-            :src="article.cover_image"
-            :alt="article.title"
-            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out"
-          />
-          <img
-            v-else
-            :src="article.social_image"
-            :alt="article.title"
-            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out"
-          />
+          <img v-if="article.cover_image" :src="article.cover_image" :alt="article.title"
+            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out" />
+          <img v-else :src="article.social_image" :alt="article.title"
+            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out" />
         </div>
 
         <!-- Title -->
         <h2 class="text-3xl font-bold md:text-7xl my-8">{{ article.title }}</h2>
         <div class="flex flex-wrap gap-2">
-          <span
-            v-for="tag in article.tags"
-            :key="tag"
-            class="text-lg text-pink-500"
-            >#{{ tag }}</span
-          >
+          <span v-for="tag in article.tags" :key="tag" class="text-lg text-pink-500">#{{ tag }}</span>
         </div>
 
         <div class="flex flex-col md:flex-row mt-10">
           <div class="md:w-1/3 text-center md:pr-8 md:py-8">
             <div class="sticky top-8">
               <!-- ============== -->
-              <div
-                class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
-              >
-                <img
-                  :src="user.profile_image"
-                  :alt="user.name"
-                  class="rounded-full"
-                />
+              <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
+                <img :src="user.profile_image" :alt="user.name" class="rounded-full" />
               </div>
-              <div
-                class="flex flex-col items-center text-center justify-center"
-              >
-                <h2
-                  class="font-medium title-font mt-4 text-gray-900 text-xl lg:text-2xl"
-                >
+              <div class="flex flex-col items-center text-center justify-center">
+                <h2 class="font-medium title-font mt-4 text-gray-900 text-xl lg:text-2xl">
                   {{ user.name }}
-                  <span class="text-base lg:text-xl text-gray-500"
-                    >@{{ user.username }}</span
-                  >
+                  <span class="text-base lg:text-xl text-gray-500">@{{ user.username }}</span>
                 </h2>
                 <div class="w-12 h-1 bg-pink-500 rounded mt-2 mb-4"></div>
-                <p
-                  v-if="user.summary"
-                  class="text-base my-4"
-                >
+                <p v-if="user.summary" class="text-base my-4">
                   <span class="text-gray-400">About:</span>
                   {{ user.summary }}
                 </p>
                 <div class="my-4">
-                  <div
-                    v-if="user.location"
-                    class="flex flex-wrap gap-2"
-                  >
+                  <div v-if="user.location" class="flex flex-wrap gap-2">
                     <div class="text-gray-400">Location:</div>
                     <div class="text-gray-800 font-bold">
                       {{ user.location }}
                     </div>
                   </div>
-                  <div
-                    v-if="user.joined_at"
-                    class="flex flex-wrap gap-2"
-                  >
+                  <div v-if="user.joined_at" class="flex flex-wrap gap-2">
                     <div class="text-gray-400">Joined:</div>
                     <div class="text-gray-800 font-bold">
                       {{ user.joined_at }}
@@ -86,52 +49,29 @@
                 <!-- Icons Linls ==============================-->
                 <div class="ml-auto mr-auto mt-4">
                   <div class="links flex justify-center items-center gap-4">
-                    <a
-                      v-if="user.github_username"
-                      :href="`https://github.com/${user.github_username}`"
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      title="Github"
-                    >
+                    <a v-if="user.github_username" :href="`https://github.com/${user.github_username}`" target="_blank"
+                      rel="nofollow noopener noreferrer" title="Github">
                       <!-- <github-icon /> -->
                       <IconGithub
-                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out"
-                      />
+                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out" />
                     </a>
-                    <a
-                      v-if="user.twitter_username"
-                      :href="`https://twitter.com/${user.twitter_username}`"
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      title="Twitter-X"
-                    >
+                    <a v-if="user.twitter_username" :href="`https://twitter.com/${user.twitter_username}`"
+                      target="_blank" rel="nofollow noopener noreferrer" title="Twitter-X">
                       <!-- <twitter-icon /> -->
                       <IconTwiter
-                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out"
-                      />
+                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out" />
                     </a>
-                    <a
-                      v-if="user.website_url"
-                      :href="user.website_url"
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      title="Website"
-                    >
+                    <a v-if="user.website_url" :href="user.website_url" target="_blank"
+                      rel="nofollow noopener noreferrer" title="Website">
                       <!-- <externallink-icon /> -->
                       <icon-web
-                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out"
-                      />
+                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out" />
                     </a>
-                    <a
-                      :href="`https://dev.to/${user.username}`"
-                      target="_blank"
-                      rel="nofollow noopener noreferrer"
-                      title="Dev.to"
-                    >
+                    <a :href="`https://dev.to/${user.username}`" target="_blank" rel="nofollow noopener noreferrer"
+                      title="Dev.to">
                       <!-- <dev-icon /> -->
                       <IconDev
-                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out"
-                      />
+                        class="w-6 h-6 hover:text-pink-700 hover:scale-125 text-pink-500 transition duration-300 ease-in-out" />
                     </a>
                   </div>
                 </div>
@@ -140,12 +80,8 @@
             </div>
           </div>
           <div
-            class="md:w-2/3 md:pl-8 md:py-8 md:border-l border-gray-200 md:border-t-0 border-t mt-4 pt-4 md:mt-0 text-left"
-          >
-            <div
-              class="content"
-              v-html="article.body_html"
-            />
+            class="md:w-2/3 md:pl-8 md:py-8 md:border-l border-gray-200 md:border-t-0 border-t mt-4 pt-4 md:mt-0 text-left">
+            <div class="content" v-html="article.body_html" />
           </div>
         </div>
       </div>
@@ -277,24 +213,37 @@ pre {
 }
 
 pre code {
-  display: block; /* Делаем элемент блочным */
-  max-width: 100%; /* Ограничиваем ширину контейнера */
-  overflow-x: auto; /* Добавляем горизонтальную прокрутку при необходимости */
-  word-break: break-all; /* Разбиваем слова при переполнении */
-  white-space: pre-wrap; /* Сохраняем перенос строк и пробелы */
+  display: block;
+  /* Делаем элемент блочным */
+  max-width: 100%;
+  /* Ограничиваем ширину контейнера */
+  overflow-x: auto;
+  /* Добавляем горизонтальную прокрутку при необходимости */
+  word-break: break-all;
+  /* Разбиваем слова при переполнении */
+  white-space: pre-wrap;
+  /* Сохраняем перенос строк и пробелы */
   padding: 10px;
-  font-family: monospace; /* Используем моноширинный шрифт */
+  font-family: monospace;
+  /* Используем моноширинный шрифт */
 }
+
 p code {
-  display: block; /* Делаем элемент блочным */
-  max-width: 100%; /* Ограничиваем ширину контейнера */
-  overflow-x: auto; /* Добавляем горизонтальную прокрутку при необходимости */
-  word-break: break-all; /* Разбиваем слова при переполнении */
-  white-space: pre-wrap; /* Сохраняем перенос строк и пробелы */
+  display: block;
+  /* Делаем элемент блочным */
+  max-width: 100%;
+  /* Ограничиваем ширину контейнера */
+  overflow-x: auto;
+  /* Добавляем горизонтальную прокрутку при необходимости */
+  word-break: break-all;
+  /* Разбиваем слова при переполнении */
+  white-space: pre-wrap;
+  /* Сохраняем перенос строк и пробелы */
   padding: 10px;
   background-color: #f5f5f5;
   border-radius: 5px;
-  font-family: monospace; /* Используем моноширинный шрифт */
+  font-family: monospace;
+  /* Используем моноширинный шрифт */
 }
 
 .highlight__panel {
