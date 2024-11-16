e1cde7aa9348cf2bafb4b5f737d1516d32ca1e67
poliweb <web@poliweb.su>
2024-11-16 10:54:47 +0600

    Update PostDetailView.vue

--- src/views/username/PostDetailView.vue
+++ src/views/username/PostDetailView.vue
@@ -1,14 +1,25 @@
 <template>
   <section class="text-gray-600 body-font">
     <div class="container px-5 py-24 mx-auto flex flex-col">
-      <div v-if="article" class="lg:w-5/6 mx-auto">
+      <div
+        v-if="article"
+        class="lg:w-5/6 mx-auto"
+      >
         <div class="rounded-lg overflow-hidden">
-          <img v-if="article.cover_image" :src="article.cover_image" :alt="article.title"
-            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out" />
-          <img v-else :src="article.social_image" :alt="article.title"
-            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out" />
+          <img
+            v-if="article.cover_image"
+            :src="article.cover_image"
+            :alt="article.title"
+            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out"
+          />
+          <img
+            v-else
+            :src="article.social_image"
+            :alt="article.title"
+            class="w-full object-cover object-center item-article__img transition duration-700 ease-in-out"
+          />
         </div>
-        
+
         <!-- Title -->
         <h2 class="text-3xl font-bold md:text-7xl my-8">{{ article.title }}</h2>
         <div class="flex flex-wrap gap-2">
@@ -16,56 +27,95 @@
             v-for="tag in article.tags"
             :key="tag"
             class="text-lg text-pink-500"
-          >#{{ tag }}</span>
+            >#{{ tag }}</span
+          >
         </div>
-
 
         <div class="flex flex-col md:flex-row mt-10">
           <div class="md:w-1/3 text-center md:pr-8 md:py-8">
             <div class="sticky top-8">
               <!-- ============== -->
-              <div class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400">
-                <img :src="user.profile_image" :alt="user.name" class="rounded-full" />
+              <div
+                class="w-40 h-40 rounded-full inline-flex items-center justify-center bg-gray-200 text-gray-400"
+              >
+                <img
+                  :src="user.profile_image"
+                  :alt="user.name"
+                  class="rounded-full"
+                />
               </div>
-              <div class="flex flex-col items-center text-center justify-center">
-                <h2 class="font-medium title-font mt-4 text-gray-900 text-xl lg:text-2xl">
+              <div
+                class="flex flex-col items-center text-center justify-center"
+              >
+                <h2
+                  class="font-medium title-font mt-4 text-gray-900 text-xl lg:text-2xl"
+                >
                   {{ user.name }}
-                  <span class="text-base lg:text-xl text-gray-500">@{{ user.username }}</span>
+                  <span class="text-base lg:text-xl text-gray-500"
+                    >@{{ user.username }}</span
+                  >
                 </h2>
                 <div class="w-12 h-1 bg-pink-500 rounded mt-2 mb-4"></div>
-                <p v-if="user.summary" class="text-base my-4">
+                <p
+                  v-if="user.summary"
+                  class="text-base my-4"
+                >
                   <span class="text-gray-400">About:</span>
                   {{ user.summary }}
                 </p>
                 <div class="my-4">
-                  <div v-if="user.location" class="flex flex-wrap gap-2">
+                  <div
+                    v-if="user.location"
+                    class="flex flex-wrap gap-2"
+                  >
                     <div class="text-gray-400">Location:</div>
-                    <div class="text-gray-800 font-bold">{{ user.location }}</div>
+                    <div class="text-gray-800 font-bold">
+                      {{ user.location }}
+                    </div>
                   </div>
-                  <div v-if="user.joined_at" class="flex flex-wrap gap-2">
+                  <div
+                    v-if="user.joined_at"
+                    class="flex flex-wrap gap-2"
+                  >
                     <div class="text-gray-400">Joined:</div>
-                    <div class="text-gray-800 font-bold">{{ user.joined_at }}</div>
+                    <div class="text-gray-800 font-bold">
+                      {{ user.joined_at }}
+                    </div>
                   </div>
                 </div>
                 <!-- Icons Linls ==============================-->
                 <div class="ml-auto mr-auto mt-4">
                   <div class="links flex justify-center items-center gap-4">
-                    <a v-if="user.github_username" :href="`https://github.com/${user.github_username}`" target="_blank">
+                    <a
+                      v-if="user.github_username"
+                      :href="`https://github.com/${user.github_username}`"
+                      target="_blank"
+                    >
                       <!-- <github-icon /> -->
-                        <IconGithub class="w-6 h-6 text-pink-500" />
-
+                      <IconGithub class="w-6 h-6 text-pink-500" />
                     </a>
-                    <a v-if="user.twitter_username" :href="`https://twitter.com/${user.twitter_username}`"
-                      target="_blank">
+                    <a
+                      v-if="user.twitter_username"
+                      :href="`https://twitter.com/${user.twitter_username}`"
+                      target="_blank"
+                    >
                       <!-- <twitter-icon /> -->
                       <IconTwiter class="w-6 h-6 text-pink-500 fill-pink-500" />
                     </a>
-                    <a v-if="user.website_url" :href="user.website_url" target="_blank"
-                      rel="nofollow noopener noreferrer">
+                    <a
+                      v-if="user.website_url"
+                      :href="user.website_url"
+                      target="_blank"
+                      rel="nofollow noopener noreferrer"
+                    >
                       <!-- <externallink-icon /> -->
                       <icon-web class="w-6 h-6 text-pink-500" />
                     </a>
-                    <a :href="`https://dev.to/${user.username}`" target="_blank" rel="nofollow noopener noreferrer">
+                    <a
+                      :href="`https://dev.to/${user.username}`"
+                      target="_blank"
+                      rel="nofollow noopener noreferrer"
+                    >
                       <!-- <dev-icon /> -->
                       <IconDev class="w-6 h-6 text-pink-500" />
                     </a>
@@ -76,8 +126,12 @@
             </div>
           </div>
           <div
-            class="md:w-2/3 md:pl-8 md:py-8 md:border-l border-gray-200 md:border-t-0 border-t mt-4 pt-4 md:mt-0 text-left">
-            <div class="content" v-html="article.body_html" />
+            class="md:w-2/3 md:pl-8 md:py-8 md:border-l border-gray-200 md:border-t-0 border-t mt-4 pt-4 md:mt-0 text-left"
+          >
+            <div
+              class="content"
+              v-html="article.body_html"
+            />
           </div>
         </div>
       </div>
@@ -92,60 +146,62 @@
   </template>
 </template>
 
-
 <script setup>
-import { ref, onMounted } from 'vue';
-import IconDev from '@/components/icons/iconDev.vue';
-import IconWeb from '@/components/icons/iconWeb.vue';
-import IconGithub from '@/components/icons/IconGithub.vue';
-import IconTwiter from '@/components/icons/iconTwiter.vue';
+import { ref, onMounted } from 'vue'
+import IconDev from '@/components/icons/iconDev.vue'
+import IconWeb from '@/components/icons/iconWeb.vue'
+import IconGithub from '@/components/icons/IconGithub.vue'
+import IconTwiter from '@/components/icons/iconTwiter.vue'
 
-const isLoading = ref(true);
+const isLoading = ref(true)
 
 // Пропсы
 const props = defineProps({
   userId: String,
   postId: String,
-});
+})
 
 // Состояние компонента
-const article = ref(null);
-const user = ref(null);
+const article = ref(null)
+const user = ref(null)
 
 // Функция для обработки ошибки
 const showNotFoundError = () => {
   if (process.server) {
-    useRouter().push({ path: '/404' }); // Редирект на страницу 404
+    useRouter().push({ path: '/404' }) // Редирект на страницу 404
   }
-  throw new Error('Статья не найдена');
-};
+  throw new Error('Статья не найдена')
+}
 
 // Запрос к API и проверка данных
 onMounted(async () => {
-  isLoading.value = true;
+  isLoading.value = true
   try {
-    const articleResponse = await fetch(`https://dev.to/api/articles/${props.postId}`);
-    const userResponse = await fetch(`https://dev.to/api/users/by_username?url=${props.userId}`);
-    const articleData = await articleResponse.json();
-    const userData = await userResponse.json();
+    const articleResponse = await fetch(
+      `https://dev.to/api/articles/${props.postId}`
+    )
+    const userResponse = await fetch(
+      `https://dev.to/api/users/by_username?url=${props.userId}`
+    )
+    const articleData = await articleResponse.json()
+    const userData = await userResponse.json()
 
     if (articleData.id && articleData.user.username === props.userId) {
-      article.value = articleData;
-      user.value = userData;
+      article.value = articleData
+      user.value = userData
       // Динамически обновляем title
-      document.title = article.value.title;
+      document.title = article.value.title
     } else {
-      showNotFoundError();
+      showNotFoundError()
     }
   } catch (error) {
-    console.error('Ошибка при загрузке статьи:', error);
-    showNotFoundError();
+    console.error('Ошибка при загрузке статьи:', error)
+    showNotFoundError()
   } finally {
-    isLoading.value = false;
+    isLoading.value = false
   }
-});
+})
 </script>
-
 
 <style>
 .content .highlight {
@@ -185,7 +241,6 @@ onMounted(async () => {
 .content a {
   color: #ec4899;
 }
-
 
 /* Простой стиль для прелоудера */
 .loader {
