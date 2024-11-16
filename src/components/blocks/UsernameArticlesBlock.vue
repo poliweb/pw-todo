bcd80aa98287bea82b5e81d67500f4e27d1b49a9
poliweb <web@poliweb.su>
2024-11-16 21:56:41 +0600

    :update src/components/blocks/UsernameArticlesBlock.vue

--- src/components/blocks/UsernameArticlesBlock.vue
+++ src/components/blocks/UsernameArticlesBlock.vue
@@ -1,22 +1,26 @@
 <template>
     <div>
-        <div class="flex flex-col items-center justify-center rounded-lg w-full h-64 bg-pink-500 overflow-hidden">
-            <!-- <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" /> -->
-             <h2 class="text-[5vw] sm:text-[2vw] md:text-[2.3vw] text-gray-100 text-center font-bold text">
-                All articles from the author 
-             </h2>
-             <h3 class="text-[8vw] sm:text-[3.5vw] md:text-[3vw] text-white text-center font-bold text">
-                {{ userArticles[0]?.user?.name }}
-             </h3>
+        <!-- Header -->
+        <div v-if="userArticles.length > 0"
+            class="flex flex-col items-center justify-center rounded-lg w-full h-64 bg-pink-500 overflow-hidden p-4">
+            <h2 class="text-[8vw] sm:text-[3.5vw] md:text-[3.5vw] text-white text-center font-bold">
+                {{ userArticles[0]?.user?.name }}`s articles - {{ userArticles.length }} total
+            </h2>
         </div>
-
+        <div v-else
+            class="flex flex-col items-center justify-center rounded-lg w-full h-64 bg-pink-500 overflow-hidden">
+            <h2 class="text-[5vw] sm:text-[2vw] md:text-[2.3vw] text-gray-100 text-center font-bold text">
+                This author has no articles
+            </h2>
+        </div>
+        <!-- || Header -->
         <div v-if="isLoading">
             <div class="flex flex-col items-center justify-center">
                 <div class="loader"></div>
                 <p>Loading articles...</p>
             </div>
         </div>
-        <div v-else-if="noArticlesFound">Статьи не найдены</div>
+        <div v-else-if="noArticlesFound">No articles found</div>
         <div v-else>
             <ul>
                 <li v-for="article in userArticles" :key="article.id">{{ article.title }}</li>
@@ -72,8 +76,9 @@ onMounted(async () => {
 
 <style scoped>
 .text {
-  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+    text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
 }
+
 .text_vw {
     font-size: 3vw;
 }
--- src/views/username/index.vue
+++ src/views/username/index.vue
@@ -1,14 +1,14 @@
 <template>
     <section class="text-gray-600 body-font">
         <div class="container px-5 py-24 mx-auto flex flex-col">
-            <div class="lg:w-5/6 mx-auto">
-                <div class="flex flex-col sm:flex-row mt-10">
-                    <div class="sm:w-1/3 text-center sm:pr-8 sm:py-8">
+            <div class="w-full lg:w-5/6 mx-auto">
+                <div class="flex flex-col md:flex-row mt-10">
+                    <div class="md:w-1/3 text-center md:pr-8 md:py-8">
                         <!-- Avatar -->
                         <UsernameBlock />
                     </div>
                     <div
-                        class="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
+                        class="md:w-2/3 md:pl-8 md:py-8 md:border-l border-gray-200 md:border-t-0 border-t mt-4 pt-4 md:mt-0 text-center md:text-left">
                         <UsernameArticlesBlock />
                     </div>
                 </div>
