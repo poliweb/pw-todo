297ab231e7316b29c5551df103ec037467d34ce0
poliweb <web@poliweb.su>
2024-11-16 17:34:50 +0600

    :update

---
+++ src/components/blocks/UsernameArticlesBlock.vue
@@ -0,0 +1,14 @@
+<template>
+    <div>
+        <div class="rounded-lg h-64 overflow-hidden">
+            <img alt="content" class="object-cover object-center h-full w-full" src="https://dummyimage.com/1200x500" />
+        </div>
+        <h1>UsernameArticlesBlock.vue</h1>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style scoped></style>
