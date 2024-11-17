1e5f25f0e7dbd837a92cdd9951ddeb33c07ffb05
poliweb <web@poliweb.su>
2024-11-17 13:29:22 +0600

    Update UsernameArticlesTotal.vue

--- src/components/blocks/UsernameArticlesTotal.vue
+++ src/components/blocks/UsernameArticlesTotal.vue
@@ -1,8 +1,12 @@
 <template>
     <div v-if="userArticlesTotal.length > 0">
-        <span class="text-[8vw] sm:text-[3.5vw] md:text-[3.5vw] 2xl:text-[70px] font-bold">articles - {{ userArticlesTotal.length }} total</span>
+        <span class="text-[8vw] sm:text-[3.5vw] md:text-[3.5vw] 2xl:text-[70px] font-bold">articles - {{
+            userArticlesTotal.length }} total</span>
     </div>
     <div v-else-if="noArticlesFound">0 total</div>
+    <div v-else class="flex flex-col items-center justify-center">
+        <p>Loading...</p>
+    </div>
 </template>
 
 
